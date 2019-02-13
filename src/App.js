8eac9c5726c86abc377dcb12ab9f516eea3b971c
benv.taa <benv.taa@gmail.com>
2019-02-13 21:22:21 +0100

    Removed Main.js in favor of LoadBootstrap.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Main from './header/Main';
+import LoadBootstrap from './header/LoadBootstrap';
 import Header from './Header';
 import Page from './Page';
 import AtomCanvas from './AtomCanvas';
@@ -107,7 +107,7 @@ class App extends Component {
   render() {
     return (
       <div id="app">
-        <Main />
+        <LoadBootstrap />
         <AtomCanvas />
         <div id="foreground">
           <Header />
