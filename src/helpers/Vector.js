829c3dc4f3bf637ff8e2649b0cdb7d7e6ed16a31
benv.taa <benv.taa@gmail.com>
2019-02-18 21:50:00 +0100

    Refactored and added extra methods

--- src/helpers/Vector.js
+++ src/helpers/Vector.js
@@ -6,25 +6,13 @@
  */
 export class Vector {
     /**
-     * If current form is component form, parameter 1 stands for the x axis and paramater 2 for the y axis.
-     * If not, then parameter 1 stands for the length and parameter 2 stands for the direction
+     * Create a new vector
      * 
-     * @param {Number} param1 
-     * @param {Number} param2 
-     * @param {Boolean} compForm Whether the parameters given are in component form 
+     * @param {Number} x 
+     * @param {Number} y 
+     * @memberof Vector
      */
-    constructor(param1, param2, compForm = true) {
-        let x;
-        let y;
-
-        if (compForm) {
-            x = param1 !== undefined ? param1 : Math.sqrt(0.5);
-            y = param2 !=- undefined ? param2 : Math.sqrt(0.5);
-        } else {
-            x = Math.cos(param2) * param1;
-            y = Math.sin(param2) * param1;
-        }
-
+    constructor(x = 1, y = 0) {
         this.x = x;
         this.y = y;
     }
@@ -61,7 +49,62 @@ export class Vector {
     }
 
     /**
-     * Get direction of the vector
+     * Scale the x component by a scalar
+     * 
+     * @param {Number} scalar
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    scaleX(scalar) {
+        return (this.x *= scalar, this);
+    }
+
+    /**
+     * Scale the y component by a scalar
+     * 
+     * @param {Number} scalar
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    scaleY(scalar) {
+        return (this.y *= scalar, this);
+    }
+
+    /**
+     * Divide the vector by a scalar
+     * 
+     * @param {Number} scalar
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    divide(scalar) {
+        this.scale(1 / scalar);
+    }
+
+    /**
+     * Divide the x component by a scalar
+     * 
+     * @param {Number} scalar
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    divideX(scalar) {
+        return (this.x /= scalar, this);
+    }
+
+    /**
+     * Divide the y component by a scalar
+     * 
+     * @param {Number} scalar
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    divideY(scalar) {
+        return (this.y /= scalar, this);
+    }
+
+    /**
+     * Get the direction of the vector
      *
      * @returns {Number}
      * @memberof Vector
@@ -80,6 +123,16 @@ export class Vector {
     setDirection(radians) {
         let mag = this.mag();
         return (this.x = mag * Math.cos(radians), this.y = mag * Math.sin(radians), this);
+    }
+
+    /**
+     * Add to the direction of the vector
+     * 
+     * @param {Number} radians
+     * @returns {Vector} The current vector 
+     */
+    addDirection(radians) {
+        return this.setDirection(this.direction() + radians);
     }
 
     /**
@@ -111,9 +164,20 @@ export class Vector {
      * @returns {Vector} The current vector
      * @memberof Vector
      */
-    normalize(scale = 1) {
+    normalize(scalar = 1) {
         let mag = this.mag();
-        return (this.x *= scale / mag, this.y *= scale / mag, this);
+        return (this.x *= scalar / mag, this.y *= scalar / mag, this);
+    }
+
+    /**
+     * Shorthand for normalize
+     *
+     * @param {Number} scale
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    norm(scalar = 1) {
+        return this.normalize(scalar);
     }
 
     /**
@@ -138,6 +202,28 @@ export class Vector {
     }
 
     /**
+     * Add a value to the x component
+     * 
+     * @param {Number} n
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    addX(n) {
+        return this.x += n, this;
+    }
+
+    /**
+     * Add a value to the y component
+     * 
+     * @param {Number} n
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    addY(n) {
+        return this.y += n, this;
+    }
+
+    /**
      * Subtract a vector
      *
      * @param {Vector} vect
@@ -149,8 +235,60 @@ export class Vector {
     }
 
     /**
+     * Subtract a value to the x component
+     * 
+     * @param {Number} n
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    subX(n) {
+        return this.x -= n, this;
+    }
+
+    /**
+     * Subtract a value to the y component
+     * 
+     * @param {Number} n
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    subY(n) {
+        return this.y -= n, this;
+    }
+
+    /**
+     * Flip the vector 180 degrees
+     * 
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    inverse() {
+        return this.scale(-1);
+    }
+
+    /**
+     * Mirror the vector relative to the x component
+     * 
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    inverseX() {
+        return this.scaleX(-1);
+    }
+
+    /**
+     * Mirror the vector relative to the y component
+     * 
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    inverseY() {    
+        return this.scaleY(-1);
+    }
+
+    /**
      * Check if one vector is parralel to another.
-     * NOTE: This may not always be trustworthy due to floating point math in programming
+     * NOTE: This may not always be trustworthy due to inaccuracies in floating point math
      *
      * @param {Vector} vect
      * @returns {Boolean}
@@ -167,7 +305,7 @@ export class Vector {
      * @returns {Number}
      * @memberof Vector 
      */
-    distanceTo(vect) {
+    distance(vect) {
         return this.clone().sub(vect).mag();
     }
 
@@ -179,7 +317,7 @@ export class Vector {
      * @memberof Vector 
      */
     distancesq(vect) {
-        return this.clone().sub()
+        return this.clone().sub(vect).magsq();
     }
     
     /**
@@ -190,12 +328,34 @@ export class Vector {
      * @memberof Vector
      */
     dotProduct(vect) {
+        return this.x * vect.x + this.y * vect.y;
+    }
 
+    /**
+     * Shorthand for dotProduct()
+     * 
+     * @param {Vector} vect
+     * @returns {Number}
+     * @memberof Vector
+     */
+    dot(vect) {
+        return this.dotProduct(vect);
+    }
+
+    /**
+     * Round the components of the vector
+     * 
+     * @returns {Vector} The current vector
+     * @memberof Vector
+     */
+    unFloat() {
+        return (this.x = Math.round(this.x), this.y = Math.round(this.y), this);
     }
 }
 
 /**
  * Relative difference in angle. Can be negative
+ * TODO: add more explenation
  * 
  * @param {Vector} vect1
  * @param {Vector} vect2
@@ -216,25 +376,12 @@ Vector.angleDifference = (vect1, vect2) => {
 };
 
 /**
- * The dot product
- * 
- * @param {Vector} vect1
- * @param {Vector} vect2
- * @returns {Number}
- */
-Vector.dotProduct = (vect1, vect2) => {
-    return vect1.x * vect2.x + vect1.y * vect2.y;
-}
-
-/**
  * Convert to radians
  * 
  * @param {Number} degree
  * @return {Number}
  */
-Vector.toRad = function (degree) {
-    return degree / 180 * Math.PI;
-}
+Vector.toRad = (degree) => degree / 180 * Math.PI;
 
 /**
  * Convert to degrees
@@ -242,6 +389,13 @@ Vector.toRad = function (degree) {
  * @param {Number} radians
  * @return {Number}
  */
-Vector.toDeg = function (radians) {
-    return radians * 180 / Math.PI;
-}
+Vector.toDeg = (radians) => radians * 180 / Math.PI;
+
+/**
+ * Create a vector from a magnitude and a direction
+ * 
+ * @param {Number} magnitude
+ * @param {Number} direction
+ * @returns {Vector}
+ */
+Vector.fromMag = (mag, dir) => new Vector(mag * Math.cos(dir), mag * Math.sin(dir));
